d48d24645371e3f6795698cae2a0e04c31d1b676
kanukv01 <130717339+kanukv01@users.noreply.github.com>
2024-04-24 09:07:55 +0000

    Upload

---
+++ Java8Features/src/predicateEx.java
@@ -0,0 +1,11 @@
+import java.util.function.*;;
+public class predicateEx{
+    public static void main(String[] args){
+        Predicate<Integer> t=I->I>10;
+        System.out.println(t.test(10));
+      //results in error  System.out.println(t.test("vinay"));
+
+      Predicate<String> p=S->S.length()>10;
+      System.out.println(p.test("vinay"));
+    }
+} 
